cfb9ede8fb5bd114669b135f4ae68bdf24b2dccc
jake <m_le@fanshaweonline.ca>
2026-02-10 10:56:51 -0500

    Added JS file with basic drag/drop

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
 console.log("JavaScript File is linked");
+
+// variables
+const dropZone = document.querySelectorAll("#diagram > div");
+const label = document.querySelectorAll(".label-title");
+const labelZone = document.querySelector("#labels");
+const button = document.querySelector("#reset-btn");
+let dragItem = null;
+
+console.log(dropZone);
+console.log(label);
+
+// functions
+function dragStart() {
+   dragItem = this;
+}
+
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function appendDrag() {
+    this.appendChild(dragItem);
+}
+
+// Event Listener
+label.forEach((elem) => {
+    elem.addEventListener("dragstart", dragStart); 
+});
+
+dropZone.forEach((elem) => {
+    elem.addEventListener("dragover", dragOver);
+    elem.addEventListener("drop", appendDrag); 
+});
