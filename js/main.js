67dc68a0e54867c288a047cdf2f8d856cca2e4d2
jake-hxf3 <m_le@fanshaweonline.ca>
2026-02-23 15:38:35 -0500

    This branch was an in class example

--- js/main.js
+++ js/main.js
@@ -1,128 +1,79 @@
 console.log("JavaScript File is linked");
 
 // variables
-const dropZone = document.querySelectorAll("#diagram > div");
-const label = document.querySelectorAll(".label-title");
-const labelZone = document.querySelector("#labels");
+const labels = document.querySelectorAll(".label");
+const targetZones = document.querySelectorAll(".target-zone");
+const labelBox = document.querySelector("#label-box");
+let currentDraggedElement = null;
 
+//add variables for the reset button
 const reset = document.querySelector("#reset-btn");
 const submit = document.querySelector("#submit-btn");
 
-const title = document.querySelector("#title");
-const inst = document.querySelector("#inst");
-const titleText = title.textContent;
-const instText = inst.textContent;
-
-let dragItem = null;
-let score = 0;
-
-console.log(dropZone);
-console.log(label);
-
 // functions
-
-// assigns element to global var once drag event starts
 function dragStart() {
-   dragItem = this;
-   setTimeout(visibleDrag, 0);
+    console.log("started dragging");
+    // whatever the user is dragging, store it into currentDraggedElement
+    currentDraggedElement = this;
 }
 
-// hide the element in the starting zone when it is dragged
-function visibleDrag() {
-    dragItem.classList.toggle("no-display");
-}
-
-// makes element drop possible but only when the drop zone is empty
 function dragOver(e) {
+    console.log("dragover called, they want their drop behavior back");
+
+    //check if the zone is empty to allow the drop
     let itemNumber = this.childElementCount;
-    if (itemNumber == 0) {
+    if(itemNumber == 0){
         e.preventDefault();
     }
 }
 
-// attaches the dragged element into a drop zone and removes highlight
-function appendDrag() {
-    this.appendChild(dragItem);
-    this.classList.remove("bg-drag");
+
+
+function dropped(e) {
+    e.preventDefault();
+
+    //prevent double drops here
+    //if this dropzone has a child then don't let it drop
+    //use a return statement
+    if(this.firstElementChild) {
+        return;
+    }
+
+
+    //drop the piece
+    this.appendChild(currentDraggedElement);
+    currentDraggedElement = null;
+    console.log("dropped");
 }
 
-// adds highlight to a drop zone when a draggable element enters it
-function changeColor(e) {
-    let itemNumber = this.childElementCount;
-    if((itemNumber == 0) || (e == "dragleave")){
-        this.classList.toggle("bg-drag");
-    } 
-}
-
-// resets game: puts the elements back to start & reverts all style changes
-function resetLabels() {
-    const diagLabel = document.querySelectorAll("#diagram .label-title");
-    this.classList.add("small-btn");
-    submit.classList.add("hidden");
-
-    score = 0;
-    title.textContent = titleText;
-    inst.textContent = instText;
-
-    diagLabel.forEach((elem) => {
-        elem.classList.remove("label-inc");
-        labelZone.appendChild(elem);
+// function resetGame() {
+    //collect all the labels and put them back
+    //check all target zones/loop through them
+    //see IF the dropzone has a label in it
+    //if it does then add the labels back to the pieces
+//}
+function resetGame() {
+    console.log("reset button clicked");
+    targetZones.forEach(zone => {
+        let zoneLabel = zone.firstElementChild;
+        if (zoneLabel) {
+            labelBox.appendChild(zoneLabel);
+        }
     })
 }
 
-// show submit button when all labels are dropped into the diagram
-function showSubmit() {
-    remLabel = labelZone.firstElementChild;
-    if(remLabel == null) {
-        reset.classList.remove("small-btn"); 
-        submit.classList.remove("hidden");
-    }
-}
 
-// submit button: change texts to notify player and hides itself once it's clicked
-function gameFunc(){
-    scoringFunc();
-    submit.classList.add("hidden"); 
 
-    inst.textContent = `You got ${score}/5 labels correct.`;
+// event listeners
+labels.forEach(label => {
+    label.addEventListener("dragstart", dragStart);
+})
 
-    if(score >= label.length){
-        title.textContent = "You Won!";
-    }
+targetZones.forEach(zone => {
+    zone.addEventListener("dragover", dragOver);
+    zone.addEventListener("drop", dropped);
+})
 
-    if(score < label.length){
-        title.textContent = "Try again.";
-    }  
-}
-
-// determines correct labels and calculates player's score
-function scoringFunc() {
-    dropZone.forEach((elem) =>{
-        let organName = elem.id;
-        let organLabel = elem.firstElementChild;
-        let labelName = organLabel.textContent.toLowerCase();
-
-        if(organName === labelName) {
-            score += 1;
-        } else {
-            organLabel.classList.add("label-inc");
-        }    
-    })
-}
-
-// Event Listener
-label.forEach((elem) => {
-    elem.addEventListener("dragstart", dragStart);
-    elem.addEventListener("dragend", visibleDrag);    
-});
-
-dropZone.forEach((elem) => {
-    elem.addEventListener("dragover", dragOver);
-    elem.addEventListener("drop", appendDrag);
-    elem.addEventListener("drop", showSubmit);
-    elem.addEventListener("dragenter", changeColor);
-    elem.addEventListener("dragleave", changeColor);
-});
-
-reset.addEventListener("click", resetLabels);
-submit.addEventListener("click", gameFunc);
+//add an eventListener to the reset button
+//listen to the click event and call a function to reset
+reset.addEventListener("click", resetGame);
